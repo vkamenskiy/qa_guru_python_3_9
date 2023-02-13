d8931f01c64ea0f2f42267335d1f137a196be975
Vladislav <djefry.kamen@gmail.com>
2023-02-13 21:42:54 +0400

    added class User

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,23 +1,24 @@
 import datetime
 
 from demoqa_tests.model.pages.practice_form import practice_form
+from demoqa_tests.model.data.user import User
 
 
 def test_successful_submit_student_registration_form():
     (
         practice_form.given_opened()
-        .fill_name('Vladislav')
-        .fill_last_name('Kamenskiy')
-        .fill_email('dje.fry@mail.ru')
-        .fill_mobile_number('9162754427')
-        .select_date_of_birth(datetime.date(1994, 9, 19))
-        .select_subjects('English')
-        .select_hobbies('Sports', 'Music')
-        .select_picture('test_pictures.webp')
-        .fill_current_address('Novotushinskiy proezd 8')
-        .select_state('Haryana')
-        .select_gender('Male')
-        .select_city('Panipat')
+        .fill_name(Vlad)
+        .fill_last_name(Vlad)
+        .fill_email(Vlad)
+        .fill_mobile_number(Vlad)
+        .select_date_of_birth(Vlad)
+        .select_subjects(Vlad)
+        .select_hobbies(Vlad)
+        .select_picture(Vlad)
+        .fill_current_address(Vlad)
+        .select_state(Vlad)
+        .select_gender(Vlad)
+        .select_city(Vlad)
         .submit_form()
     )
 
@@ -33,3 +34,19 @@ def test_successful_submit_student_registration_form():
         'Novotushinskiy proezd 8',
         'Haryana Panipat'
     )
+
+
+Vlad = User(
+    "Vladislav",
+    "Kamenskiy",
+    "dje.fry@mail.ru",
+    "Male",
+    "9162754427",
+    datetime.date(1994, 9, 19),
+    "English",
+    ("Sports", "Music"),
+    "test_pictures.webp",
+    "Novotushinskiy proezd 8",
+    "Haryana",
+    "Panipat",
+)
